3e41f88a8b9e40a0efb712e8dbf4dd3c059660e6
HaabuK <karl.h.haabu@gmail.com>
2023-12-18 10:04:38 +0200

    Folders, Appsettings, Dto and Context

---
+++ CarsApp/CarsApp/Data/CarsAppContext.cs
@@ -0,0 +1,17 @@
+﻿using CarsApp.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarsApp.Data
+{
+	public class CarsAppContext : IdentityDbContext<ApplicationUser>
+    {
+		public CarsAppContext(DbContextOptions<CarsAppContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
+
---
+++ CarsApp/CarsApp/Domain/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CarsApp.Domain
+{
+	public class Car
+	{
+        public Guid? Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public DateOnly Year { get; set; }
+        public string Color { get; set; }
+        public int Power { get; set; }
+        public string Fuel { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
+
