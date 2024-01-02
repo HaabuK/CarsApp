c36593f5f9e71ad010df1db79f17a8fddbb1881e
HaabuK <karl.h.haabu@gmail.com>
2024-01-02 09:53:41 +0200

    Configure TestBase

--- CarsApp/CarsAppTest/Mock/MockIHostEnvironment.cs
+++ CarsApp/CarsAppTest/Mock/MockIHostEnvironment.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+
 namespace CarsAppTest.Mock
 {
-	public class MockIHostEnvironment
-	{
-		public MockIHostEnvironment()
-		{
-		}
-	}
+    public class MockIHostEnvironment : IHostEnvironment
+    {
+        public string EnvironmentName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ContentRootPath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IFileProvider ContentRootFileProvider { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string IHostEnvironment.ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
 }
 
--- CarsApp/CarsAppTest/TestBase.cs
+++ CarsApp/CarsAppTest/TestBase.cs
@@ -1,11 +1,68 @@
 ﻿using System;
+using CarsApp.Data;
+using CarsApp.ServiceInterface;
+using CarsApp.Services;
+using CarsAppTest.Macros;
+using CarsAppTest.Mock;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace CarsAppTest
 {
-	public class TestBase
-	{
-		public TestBase()
-		{
-		}
-	}
+    public abstract class TestBase
+    {
+        protected IServiceProvider serviceProvider { get; set; }
+
+        protected TestBase()
+        {
+            var services = new ServiceCollection();
+            SetupServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        protected T Svc<T>()
+        {
+            return serviceProvider.GetService<T>();
+        }
+
+        protected T Macro<T>() where T : IMacros
+        {
+            return serviceProvider.GetService<T>();
+        }
+
+        public virtual void SetupServices(IServiceCollection services)
+        {
+            services.AddScoped<ICarsServices, CarsServices>();
+            services.AddScoped<IHostEnvironment, MockIHostEnvironment>();
+
+            services.AddDbContext<CarsAppContext>(x =>
+            {
+                x.UseInMemoryDatabase("TEST");
+                x.ConfigureWarnings(e => e.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+            });
+
+            RegisterMacros(services);
+        }
+
+        private void RegisterMacros(IServiceCollection services)
+        {
+            var macroBaseType = typeof(IMacros);
+
+            var macros = macroBaseType.Assembly.GetTypes().Where(x => macroBaseType.IsAssignableFrom(x) && !x.IsInterface
+            && !x.IsAbstract);
+
+            foreach (var macro in macros)
+            {
+                services.AddSingleton(macro);
+            }
+        }
+    }
 }
 
